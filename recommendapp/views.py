c09abff3c6e6135675e57c0f665c9075dd56b9e0
dhkang.bsn <dhkang.bsn@gmail.com>
2021-11-28 22:12:31 +0900

    21-11-25 34 Music Recommend API Merge 4

--- recommendapp/views.py
+++ recommendapp/views.py
@@ -11,20 +11,18 @@ def main_html(request):
     return render(request, 'recommendapp/main_html.html', {})
 
 URL = 'http://15.164.232.127:5000/recommend'
-headers = {'User-Agent': 'Mozila/5.0'}
+URL = 'http://127.0.0.1:5000/recommend'
+headers = { 'User-Agent' : 'Mozila/5.0' }
 
 def recommend(request):
     try:
         name = request.POST['name']
-        print('name', name)
         payload = {'name': str(name)}
         session = requests.Session()
         res = session.post(URL, headers=headers, data=payload)
-        print(res.text)
         result = json.loads(res.text)
-        print(result)
         session.close()
-        return render(request, 'recommendapp/recommend.html', {'result':result['emotion_title']})
+        return render(request, 'recommendapp/recommend.html', { 'result' : result['recommend_list']})
     except:
         return render(request, 'recommendapp/main_html.html', {})
 
